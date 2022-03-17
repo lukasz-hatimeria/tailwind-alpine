ceecb6357f2c03fe9518b34199000b046bdb9fa1
Łukasz Baran <lukasz.baran@hatimeria.pl>
2022-03-17 10:44:57 +0100

    fix slider, add new logic, refactor code

--- index/tailwind.config.js
+++ index/tailwind.config.js
@@ -4,9 +4,20 @@ module.exports = {
         extend: {},
         colors: {
             'my-yellow': '#cea600',
-            'my-grey': '#262626',
+            'my-grey': 'rgba(38, 38, 38, 0.61)',
             'my-white': '#ffffff',
         },
+        minHeight: {
+            100: '6.25rem',
+            200: '12.5rem',
+        },
+        maxHeight: {
+            100: '6.25rem',
+            200: '12.5rem',
+        },
+        minWidth: {
+            160: '9.3rem',
+        }
     },
     plugins: [],
 };
