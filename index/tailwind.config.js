616f427ad5cbd24d14fc87ce3e808737bdc9bb3c
Łukasz Baran <lukasz.baran@hatimeria.pl>
2022-03-17 09:37:13 +0100

    fix AddToCart

--- index/tailwind.config.js
+++ index/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {},
+        colors: {
+            'my-yellow': '#cea600',
+            'my-grey': '#262626',
+            'my-white': '#ffffff',
+        },
+    },
+    plugins: [],
+};
+//'#cea600',
